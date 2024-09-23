ff43dfd93033f157c21e0bec60fbae7f7900484d
stopmin <geemin0@naver.com>
2024-09-23 14:40:52 +0900

    feat: FarmResponse 생성 #2

---
+++ src/main/java/poomasi/domain/farm/dto/FarmResponse.java
@@ -0,0 +1,24 @@
+package poomasi.domain.farm.dto;
+
+import poomasi.domain.farm.entity.Farm;
+
+
+public record FarmResponse( // FIXME: 사용자 정보 추가 및 설명/전화번호 추가
+                            Long id,
+                            String name,
+                            String address,
+                            String addressDetail,
+                            Double latitude,
+                            Double longitude
+) {
+    public static FarmResponse fromEntity(Farm farm) {
+        return new FarmResponse(
+                farm.getId(),
+                farm.getName(),
+                farm.getAddress(),
+                farm.getAddressDetail(),
+                farm.getLatitude(),
+                farm.getLongitude()
+        );
+    }
+}
