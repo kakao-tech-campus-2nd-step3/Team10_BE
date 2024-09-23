c1379577bb4a42e30abeb293e43f4098d3af3b25
stopmin <geemin0@naver.com>
2024-09-23 14:32:34 +0900

    feat: FarmManageService 정의 - 등록 인터페이스만 정의 #2

---
+++ src/main/java/poomasi/domain/farm/service/FarmManageService.java
@@ -0,0 +1,21 @@
+package poomasi.domain.farm.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import poomasi.domain.farm.dto.FarmRegisterRequest;
+import poomasi.domain.farm.repository.FarmRepository;
+
+@Service
+@AllArgsConstructor
+public class FarmManageService {
+    private final FarmRepository farmRepository;
+
+    public Long registerFarm(FarmRegisterRequest request) {
+        // TODO: 판매자 인가?
+
+        // TODO: 이미 등록된 농장 인가?
+
+
+        return farmRepository.save(request.toEntity()).getId();
+    }
+}
