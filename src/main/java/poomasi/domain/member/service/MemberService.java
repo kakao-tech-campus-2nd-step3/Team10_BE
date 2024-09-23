4483f99ef9f38bb5ae4af28a0098eaa191982cf3
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-09-23 15:05:36 +0900

    feat: 로그인 에러 처리 #11

--- src/main/java/poomasi/domain/member/service/MemberService.java
+++ src/main/java/poomasi/domain/member/service/MemberService.java
@@ -7,12 +7,15 @@ import poomasi.domain.member.dto.request.LoginRequest;
 import poomasi.domain.member.dto.response.LoginResponse;
 import poomasi.domain.member.repository.MemberRepository;
 import poomasi.domain.member.entity.Member;
+import poomasi.global.error.BusinessException;
 import poomasi.global.util.JwtUtil;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static poomasi.domain.member.entity.LoginType.LOCAL;
+import static poomasi.global.error.BusinessError.INVALID_CREDENTIAL;
+import static poomasi.global.error.BusinessError.MEMBER_NOT_FOUND;
 
 @Service
 public class MemberService {
@@ -28,10 +31,10 @@ public class MemberService {
 
     public LoginResponse login(LoginRequest loginRequest) {
         Member member = memberRepository.findByEmailAndLoginType(loginRequest.email(), LOCAL)
-                .orElseThrow(() -> new MemberNotFoundExeption(MEMBER_NOT_FOUND));
+                .orElseThrow(() -> new BusinessException(MEMBER_NOT_FOUND));
 
         if (!new BCryptPasswordEncoder().matches(loginRequest.password(), member.getPassword())) {
-            throw new InvalidCredentialsException(INVALID_CREDENTIAL);
+            throw new BusinessException(INVALID_CREDENTIAL);
         }
 
         Map<String, Object> claims = new HashMap<>();
--- src/main/java/poomasi/global/error/BusinessError.java
+++ src/main/java/poomasi/global/error/BusinessError.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum BusinessError {
-	EXAMPLE_ERROR(HttpStatus.BAD_REQUEST, "에러 예시입니다.");
+	EXAMPLE_ERROR(HttpStatus.BAD_REQUEST, "에러 예시입니다."),
+	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원이 존재하지 않습니다."),
+	INVALID_CREDENTIAL(HttpStatus.UNAUTHORIZED, "잘못된 비밀번호 입니다.");
 
 	private final HttpStatus httpStatus;
 
