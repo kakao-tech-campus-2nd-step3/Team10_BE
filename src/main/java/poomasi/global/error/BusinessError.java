8125b10c9e18ee325f89f2ca2d743d373b2904a9
amm0124 <amm012450@gmail.com>
2024-09-24 20:18:54 +0900

    feat : member error 추가

--- src/main/java/poomasi/global/error/BusinessError.java
+++ src/main/java/poomasi/global/error/BusinessError.java
@@ -8,7 +8,16 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum BusinessError {
-	EXAMPLE_ERROR(HttpStatus.BAD_REQUEST, "에러 예시입니다.");
+	EXAMPLE_ERROR(HttpStatus.BAD_REQUEST, "에러 예시입니다."),
+
+
+	// Member Error
+	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버를 찾지 못했습니다."),
+	MEMBER_NOT_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "인증이 되지 않았습니다."),
+	MEMBER_NOT_AUTHORIZATION(HttpStatus.FORBIDDEN, "권한이 없습니다.")
+
+
+	;
 
 	private final HttpStatus httpStatus;
 
