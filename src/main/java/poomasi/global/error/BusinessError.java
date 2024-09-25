ec46e99ea5780baa83bc6b406f70e96e17eb984c
stopmin <geemin0@naver.com>
2024-09-25 13:20:45 +0900

    feat: 예시 에러 삭제 #2

--- src/main/java/poomasi/global/error/BusinessError.java
+++ src/main/java/poomasi/global/error/BusinessError.java
@@ -8,8 +8,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum BusinessError {
-    EXAMPLE_ERROR(HttpStatus.BAD_REQUEST, "에러 예시입니다."),
-
     // Farm
     FARM_NOT_FOUND(HttpStatus.NOT_FOUND, "농장을 찾을 수 없습니다."),
     ;
