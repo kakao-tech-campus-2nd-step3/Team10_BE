0093985a202377f750a2ffc6985e473d7d409c42
stopmin <geemin0@naver.com>
2024-09-25 13:17:58 +0900

    feat: 농장 목록 조회 #2

--- src/main/java/poomasi/domain/farm/controller/FarmController.java
+++ src/main/java/poomasi/domain/farm/controller/FarmController.java
@@ -1,6 +1,7 @@
 package poomasi.domain.farm.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import poomasi.domain.farm.service.FarmService;
@@ -17,9 +18,8 @@ public class FarmController {
         return ResponseEntity.ok(farmService.getFarmByFarmId(farmId));
     }
 
-    // 농장 조회 페이지네이션
     @GetMapping("")
-    public ResponseEntity<?> getFarmList(@RequestParam int page, @RequestParam int size) {
-        return ResponseEntity.ok(farmService.getFarmList(page, size));
+    public ResponseEntity<?> getFarmList(Pageable pageable) {
+        return ResponseEntity.ok(farmService.getFarmList(pageable));
     }
 }
--- src/main/java/poomasi/domain/farm/service/FarmService.java
+++ src/main/java/poomasi/domain/farm/service/FarmService.java
@@ -1,11 +1,15 @@
 package poomasi.domain.farm.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import poomasi.domain.farm.dto.FarmResponse;
 import poomasi.domain.farm.repository.FarmRepository;
 import poomasi.global.error.BusinessError;
 import poomasi.global.error.BusinessException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -19,4 +23,9 @@ public class FarmService {
     }
 
 
+    public List<FarmResponse> getFarmList(Pageable pageable) {
+        return farmRepository.findAll(pageable).stream()
+                .map(FarmResponse::fromEntity)
+                .collect(Collectors.toList());
+    }
 }
