7e2693d72d847a387758b88938b33557bb1f202a
amm0124 <amm012450@gmail.com>
2024-09-27 03:06:34 +0900

    feat : member repository 메서드 추가

--- src/main/java/poomasi/domain/member/repository/MemberRepository.java
+++ src/main/java/poomasi/domain/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package poomasi.domain.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import poomasi.domain.member.entity.Member;
 
-public interface MemberRepository extends JpaRepository <Member, Long> {
+import java.util.Optional;
 
+public interface MemberRepository extends JpaRepository <Member, Long> {
+    Optional<Member> findByUsername(String username);
 }
