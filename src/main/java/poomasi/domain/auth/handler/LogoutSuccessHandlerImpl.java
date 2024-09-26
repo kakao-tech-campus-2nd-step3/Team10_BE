5b65d5648089e28c5e4228369c724753f937c195
amm0124 <amm012450@gmail.com>
2024-09-26 14:37:14 +0900

    feat : 로그아웃 성공시, "/" redirect하는 handler 구현 - 경로는 차후 바뀜 (임시 경로)

---
+++ src/main/java/poomasi/domain/auth/handler/LogoutSuccessHandlerImpl.java
@@ -0,0 +1,22 @@
+package poomasi.domain.auth.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        //TODO : redis blacklist 구현해야 함
+        log.info("[LogoutSuccessHandler] : Log - out successful!");
+        response.sendRedirect("/");
+    }
+}
