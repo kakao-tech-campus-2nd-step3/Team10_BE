e75f55b12e141b19a8bc6783adb4d516720de5a6
amm0124 <amm012450@gmail.com>
2024-09-26 14:34:28 +0900

    refactoring : success handler log 추가

--- src/main/java/poomasi/domain/auth/handler/AuthenticationSuccessHandlerImpl.java
+++ src/main/java/poomasi/domain/auth/handler/AuthenticationSuccessHandlerImpl.java
@@ -3,6 +3,7 @@ package poomasi.domain.auth.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -10,6 +11,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import java.io.IOException;
 
 // login 성공 시, authentication 객체를 context에 저장해주는 핸들러
+@Slf4j
 public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
@@ -19,6 +21,7 @@ public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHa
         SecurityContextHolder.getContext().
                 setAuthentication(authentication);
 
+        log.info("[AuthenticationSuccessHandler] : Authentication(Log - in) successful!");
         response.sendRedirect("/");
     }
 }
