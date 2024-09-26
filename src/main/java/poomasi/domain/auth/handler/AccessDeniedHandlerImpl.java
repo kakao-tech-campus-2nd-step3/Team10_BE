e1cc09d191113f7131a3c4e7eb8c8b689f945f43
amm0124 <amm012450@gmail.com>
2024-09-27 03:12:17 +0900

    feat : AccessDeniedHandler 구현

---
+++ src/main/java/poomasi/domain/auth/handler/AccessDeniedHandlerImpl.java
@@ -0,0 +1,16 @@
+package poomasi.domain.auth.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
+    }
+}
