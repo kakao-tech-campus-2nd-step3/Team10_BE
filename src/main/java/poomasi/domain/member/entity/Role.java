b8db78917e60aadd324211de51b955813576083d
amm0124 <amm012450@gmail.com>
2024-09-27 03:07:02 +0900

    feat : Role을 String 형태로 return하는 메서드 추가

--- src/main/java/poomasi/domain/member/entity/Role.java
+++ src/main/java/poomasi/domain/member/entity/Role.java
@@ -4,9 +4,14 @@ import org.springframework.security.core.GrantedAuthority;
 
 
 //extends GrantedAuthority를 해서 구현한 것도 있던데 . . . 좀 더 찾아봐야겠어요
-public enum Role {
+public enum Role implements GrantedAuthority {
     ROLE_ADMIN,       // 관리자
     ROLE_FARMER,      // 농부 역할
-    ROLE_CUSTOMER     // 구매자 역할
+    ROLE_CUSTOMER;     // 구매자 역할
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
 
