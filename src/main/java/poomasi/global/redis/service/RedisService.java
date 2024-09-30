7a39b6e9ea8a0be49c88931daf6c22fee74603fc
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-09-30 10:42:04 +0900

    feat: Redis 블랙리스트 로직 추가 #21

--- src/main/java/poomasi/global/redis/service/RedisService.java
+++ src/main/java/poomasi/global/redis/service/RedisService.java
@@ -18,6 +18,7 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class RedisService {
     private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisTemplate<String, Object> redisBlackListTemplate;
 
     public void setValues(String key, String data, Duration duration) {
         ValueOperations<String, Object> values = redisTemplate.opsForValue();
@@ -35,6 +36,31 @@ public class RedisService {
 
     public void deleteValues(String key) {
         redisTemplate.delete(key);
+    }
+
+    public boolean hasKey(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+    public void setBlackList(String key, String data, Duration duration) {
+        ValueOperations<String, Object> values = redisBlackListTemplate.opsForValue();
+        values.set(key, data, duration);
+    }
+
+    public String getBlackList(String key) {
+        ValueOperations<String, Object> values = redisBlackListTemplate.opsForValue();
+        if (values.get(key) == null) {
+            return "false";
+        }
+        return (String) values.get(key);
+    }
+
+    public void deleteBlackList(String key) {
+        redisBlackListTemplate.delete(key);
+    }
+
+    public boolean hasKeyBlackList(String key) {
+        return Boolean.TRUE.equals(redisBlackListTemplate.hasKey(key));
     }
 
     public List<String> getKeysByPattern(String pattern){
