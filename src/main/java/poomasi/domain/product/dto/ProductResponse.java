20479534523fab330b8d99963c657556108f67e7
stopmin <geemin0@naver.com>
2024-10-02 21:04:48 +0900

    refactor: 중복코드 삭제 #36

--- src/main/java/poomasi/domain/product/dto/ProductResponse.java
+++ src/main/java/poomasi/domain/product/dto/ProductResponse.java
@@ -24,27 +24,4 @@ public record ProductResponse(
                 .categoryId(product.getCategoryId())
                 .build();
     }
-
-    @Builder
-    public static record ProductResponse(
-            long id,
-            String name,
-            Long price,
-            int stock,
-            String description,
-            String imageUrl,
-            long categoryId
-    ) {
-        public static ProductResponse fromEntity(Product product) {
-            return ProductResponse.builder()
-                    .id(product.getId())
-                    .name(product.getName())
-                    .price(product.getPrice())
-                    .stock(product.getStock())
-                    .description(product.getDescription())
-                    .imageUrl(product.getImageUrl())
-                    .categoryId(product.getCategoryId())
-                    .build();
-        }
-    }
 }
