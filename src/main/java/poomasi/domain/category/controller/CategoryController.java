1e7b42f19326c868b86d0dd9305f261812c32a42
canyos <4581974@naver.com>
2024-10-03 16:54:24 +0900

    feat: 카테고리 내 상품 조회

--- src/main/java/poomasi/domain/category/controller/CategoryController.java
+++ src/main/java/poomasi/domain/category/controller/CategoryController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import poomasi.domain.category.dto.CategoryResponse;
+import poomasi.domain.category.dto.ProductListInCategoryResponse;
 import poomasi.domain.category.service.CategoryService;
 
 import java.util.List;
@@ -19,5 +21,11 @@ public class CategoryController {
     public ResponseEntity<?> getAllCategories() {
         List<CategoryResponse> categories = categoryService.getAllCategories();
         return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    @GetMapping("/api/categories/{categoryId}")
+    public ResponseEntity<?> getCategoryById(@PathVariable long categoryId) {
+        List<ProductListInCategoryResponse> productList = categoryService.getProductInCategory(categoryId);
+        return new ResponseEntity<>(productList, HttpStatus.OK);
     }
 }
--- src/main/java/poomasi/domain/category/service/CategoryService.java
+++ src/main/java/poomasi/domain/category/service/CategoryService.java
@@ -4,13 +4,15 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import poomasi.domain.category.dto.CategoryResponse;
+import poomasi.domain.category.dto.ProductListInCategoryResponse;
 import poomasi.domain.category.entity.Category;
 import poomasi.domain.category.repository.CategoryRepository;
+import poomasi.global.error.BusinessError;
+import poomasi.global.error.BusinessException;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
-
     private final CategoryRepository categoryRepository;
 
     public List<CategoryResponse> getAllCategories() {
@@ -18,5 +20,16 @@ public class CategoryService {
         return categories.stream()
                 .map(CategoryResponse::fromEntity)
                 .toList();
+    }
+
+    public List<ProductListInCategoryResponse> getProductInCategory(long categoryId) {
+        Category category = getCategory(categoryId);
+        return category.getProducts().stream()
+                .map(ProductListInCategoryResponse::fromEntity).toList();
+    }
+
+    private Category getCategory(long categoryId){
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(()->new BusinessException(BusinessError.CATEGORY_NOT_FOUND));
     }
 }
