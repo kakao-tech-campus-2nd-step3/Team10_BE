85e610aec62cfa0c4648bbbef5477a6262c24908
정지민 <108014449+stopmin@users.noreply.github.com>
2024-10-07 20:08:04 +0900

    S3 설정 및 Presigned URL 관련 메소드 정의 (#51)
    * feat: S3 Properties 설정
    - S3Config 정의
    - s3Presigner Bean으로 관리
    #32
    
    * feat: S3PresignedUrl
    - Presgiend Url 발급 메소드
    #32
    
    * refactor: AwsProperties 네이밍 수정
    #32
    
    * refactor: 패키지 분리
    - aws/s3 분리
    #32
    
    * refactor: EncryptionUtil 분리
    #32
    
    * S3 presignedUrl 테스트 코드 작성 #32
    
    * feat: 테스트 API
    #32
    
    ---------
    
    Co-authored-by: 정진택 <87135698+jjt4515@users.noreply.github.com>

---
+++ src/main/java/poomasi/global/config/aws/AwsProperties.java
@@ -0,0 +1,20 @@
+package poomasi.global.config.aws;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties("aws")
+public class AwsProperties {
+    private String access;
+    private String secret;
+    private S3Properties s3;
+
+    @Data
+    public static class S3Properties {
+        private String bucket;
+        private String region;
+    }
+}
---
+++ src/test/java/poomasi/global/config/s3/S3PresignedUrlServiceTest.java
@@ -0,0 +1,76 @@
+package poomasi.global.config.s3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import poomasi.global.config.aws.AwsProperties;
+import poomasi.global.util.EncryptionUtil;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class S3PresignedUrlServiceTest {
+
+    private S3PresignedUrlService s3PresignedUrlService;
+
+    @Autowired
+    private AwsProperties awsProperties;
+
+    @BeforeEach
+    public void setUp() {
+        String accessKey = awsProperties.getAccess();
+        String secretKey = awsProperties.getSecret();
+        String region = awsProperties.getS3().getRegion();
+
+        // 자격 증명 설정
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
+                accessKey,
+                secretKey
+        );
+
+        // S3Presigner 인스턴스 생성
+        S3Presigner presigner = S3Presigner.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .region(Region.of(region))
+                .build();
+
+        // S3PresignedUrlService 초기화
+        s3PresignedUrlService = new S3PresignedUrlService(presigner, new EncryptionUtil());
+    }
+
+    @Test
+    public void testCreatePresignedGetUrl() {
+        String objectKey = "object_key";
+        String bucketName = awsProperties.getS3().getBucket();
+
+        String presignedUrl = s3PresignedUrlService.createPresignedGetUrl(bucketName, objectKey);
+
+        assertNotNull(presignedUrl);
+        System.out.println("Presigned GET URL: " + presignedUrl);
+    }
+
+    @Test
+    public void testCreatePresignedPutUrl() {
+        String keyPrefix = "uploads";
+        String bucketName = awsProperties.getS3().getBucket();
+
+        // 메타데이터 생성
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", "image/jpg");
+        metadata.put("x-amz-meta-title", "Test Image");
+
+        // presigned PUT URL 생성
+        String presignedUrl = s3PresignedUrlService.createPresignedPutUrl(bucketName, keyPrefix, metadata);
+
+        assertNotNull(presignedUrl);
+        System.out.println("Presigned PUT URL: " + presignedUrl);
+    }
+}
