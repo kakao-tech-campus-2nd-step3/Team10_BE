9233f3279f17ea36c504c5704ef43e28585578d6
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-10-12 22:29:35 +0900

    refactor: EncryptionUtil 인코딩 변수 추가 #33

--- src/main/java/poomasi/global/util/EncryptionUtil.java
+++ src/main/java/poomasi/global/util/EncryptionUtil.java
@@ -8,13 +8,15 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.time.LocalDateTime;
 import java.util.Base64;
+import java.util.UUID;
 
 @Component
 public class EncryptionUtil {
 
     public String encodeTime(LocalDateTime time) {
         try {
-            String timeString = time.toString();
+            String uniqueSuffix = UUID.randomUUID().toString(); // 추가 요소
+            String timeString = time.toString() + uniqueSuffix;
             byte[] hash = MessageDigest.getInstance("SHA-256")
                     .digest(timeString.getBytes(StandardCharsets.UTF_8));
             return Base64.getUrlEncoder().withoutPadding().encodeToString(hash);
